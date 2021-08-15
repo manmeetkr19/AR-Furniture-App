48202981f6b34d1511425f3a17dd54120f7a9779
manmeetkr19 <66664358+manmeetkr19@users.noreply.github.com>
2021-08-15 09:33:59 +0530

    Scriptable object for each furniture

---
+++ Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Item1",menuName ="AddItem/Item")]
+public class Item : ScriptableObject
+{
+    public float price;
+    public GameObject furniturePrefab;
+    public Sprite furnitureImage;
+   
+}
